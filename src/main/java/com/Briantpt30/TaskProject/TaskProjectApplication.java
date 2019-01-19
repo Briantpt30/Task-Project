3032e27e231b055b254309a5690fc37ac5c08f13
Brian Quezada <briantpt30@gmail.com>
2019-01-19 09:44:56 -0500

    initial commit

---
+++ src/main/java/com/Briantpt30/TaskProject/TaskProjectApplication.java
@@ -0,0 +1,16 @@
+package com.Briantpt30.TaskProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class TaskProjectApplication {
+
+	public static void main(String[] args) {
+		run(TaskProjectApplication.class, args);
+	}
+
+}
+
