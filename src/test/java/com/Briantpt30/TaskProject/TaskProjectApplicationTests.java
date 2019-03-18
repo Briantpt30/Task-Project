13dab0c30700c90ef57df4abe25bd4c4ad783830
Brian Quezada <briantpt30@gmail.com>
2019-03-18 16:43:59 -0400

    merge branch

---
+++ src/test/java/com/Briantpt30/TaskProject/TaskProjectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.Briantpt30.TaskProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
